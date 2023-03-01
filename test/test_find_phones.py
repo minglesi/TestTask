a21523f99d6e801bb7db2c79e01036d1a277160f
Matias <minglesi@gmail.com>
2023-03-01 01:45:51 -0300

    Final Commit

--- test/test_find_phones.py
+++ test/test_find_phones.py
@@ -6,60 +6,60 @@ class TestFindPhones(unittest.TestCase):
     def test_find_phones(self):
         text = "+1 812 9874561"
         result = find_phones(text)
-        expected = {"+1 (812) 987-4561"}
+        expected = {18129874561}
         self.assertEqual(expected, result)
 
         text = "+2 (812) 123-45-67"
         result = find_phones(text)
-        expected = {"+2 (812) 123-4567"}
+        expected = {28121234567}
         self.assertEqual(expected, result)
 
         text = "+3 812 123-4567"
         result = find_phones(text)
-        expected = {"+3 (812) 123-4567"}
+        expected = {38121234567}
         self.assertEqual(expected, result)
 
         text = "+47581234567"
         result = find_phones(text)
-        expected = {"+4 (758) 123-4567"}
+        expected = {47581234567}
         self.assertEqual(expected, result)
 
         text = "+5812 1234567"
         result = find_phones(text)
-        expected = {"+5 (812) 123-4567"}
+        expected = {58121234567}
         self.assertEqual(expected, result)
 
         text = "+6-812-123-45-67"
         result = find_phones(text)
-        expected = {"+6 (812) 123-4567"}
+        expected = {68121234567}
         self.assertEqual(expected, result)
 
         text = "(712) 123-4567"
         result = find_phones(text)
-        expected = {"+7 (712) 123-4567"}
+        expected = {77121234567}
         self.assertEqual(expected, result)
 
         text = "812 1234567"
         result = find_phones(text)
-        expected = {"+7 (812) 123-4567"}
+        expected = {78121234567}
         self.assertEqual(expected, result)
 
         text = "915-1234567"
         result = find_phones(text)
-        expected = {"+7 (915) 123-4567"}
+        expected = {79151234567}
         self.assertEqual(expected, result)
 
         text = "987-6543"
         result = find_phones(text)
-        expected = {"+7 (812) 987-6543"}
+        expected = {78129876543}
         self.assertEqual(expected, result)
 
         text = "876-54-32"
         result = find_phones(text)
-        expected = {"+7 (812) 876-5432"}
+        expected = {78128765432}
         self.assertEqual(expected, result)
 
         text = "7654321"
         result = find_phones(text)
-        expected = {"+7 (812) 765-4321"}
+        expected = {78127654321}
         self.assertEqual(expected, result)
